35946cb60863a9cb8cc518d3338031485b70182d
Foiac <kaio_cfs@hotmail.com>
2025-05-01 15:42:38 -0300

    First commit

--- app/domain/models/prompt.py
+++ app/domain/models/prompt.py
@@ -3,10 +3,4 @@ from dataclasses import dataclass
 @dataclass
 class Prompt:
     user_input: str
-    context: str = """
-    You are a helpful agent who can talk to users about the weather.¨
-
-    - ATENTION
-
-    1 - You will to talk ONLY about weather
-    """
+    context: str = ""
